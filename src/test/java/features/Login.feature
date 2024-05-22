1eb9e3f28fa542264a3c78f8913b37100c8fd6a6
CHANDRA SHEKHAR CHIRRA <chandu96qa@gmail.com>
2024-05-22 15:03:24 +0530

    removed practiced one

--- src/test/java/features/Login.feature
+++ src/test/java/features/Login.feature
@@ -1,5 +1,1 @@
-Feature: Application login
-  Scenario: Home page login Test
-    Given  user is on landing page
-    When user login in to application with username and password
-    Then user should navigate to Homepage
+
--- src/test/java/stepDefination/StepDefination.java
+++ src/test/java/stepDefination/StepDefination.java
@@ -4,19 +4,4 @@ import io.cucumber.java.en.*;
 
 public class StepDefination {
 
-    @Given("user is on landing page")
-    public void user_is_on_landing_page() {
-        System.out.println("landing page");
-    }
-
-    @When("user login in to application with username and password")
-    public void user_login_in_to_application_with_username_and_password() {
-        System.out.println("home page");
-    }
-
-    @Then("user should navigate to Homepage")
-    public void user_should_navigate_to_homepage() {
-        System.out.println("logout page");
-    }
-
 }
